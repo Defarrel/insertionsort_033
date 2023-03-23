c24853c2fabbf5f078b663e88c32658224d73833
defarrel <Defarrel>
2023-03-23 10:47:34 +0700

    membuat algoritma insertionsort

--- insertionsort/insertionsort.cpp
+++ insertionsort/insertionsort.cpp
@@ -26,3 +26,20 @@ void input() {
 	}
 }
 
+void insetionsort() {
+	int i, key, j;
+	for (i = 1; i < n; i++) {
+		key = a[i];
+		j = i - 1;
+
+		while (j >= 0 && a[j] > key) {
+			a[j + 1] = a[j];
+			j = j - 1;
+		}
+		a[j + 1] = key;
+	}
+}
+int main() {
+
+
+}
